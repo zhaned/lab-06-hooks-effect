30524b6537023913844796f82d0cd5de8de3a3ce
zhaned <zhanedmond@gmail.com>
2021-05-19 10:12:48 -0700

    testing for list page passing

--- src/containers/AnimalCrossing.test.jsx
+++ src/containers/AnimalCrossing.test.jsx
@@ -3,18 +3,27 @@ import { render, screen } from '@testing-library/react';
 import AnimalCrossing from './AnimalCrossing';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
+import mockData from './villagers.json';
 
 const server = setupServer(
   rest.get('https://ac-vill.herokuapp.com/villagers', (req, res, ctx) => {
-    return res(ctx.json())
+    return res(ctx.json(mockData))
   })
 )
 
 describe('AC container', () => {
+  beforeAll(()=> server.listen());
+  afterAll(() => server.close());
+
+  it('shows a loading screen that says Wrangling Villagers...', async () => {
+    render(<AnimalCrossing />)
+
+    screen.getByText('Wrangling Villagers...');
+  })
+
   it('gives a list of villagers', async () => {
     render(<AnimalCrossing />)
 
-    screen.getByText('Loading');
     const unorderedList = await screen.findByRole('list', { name: 'villagers' });
     expect(unorderedList).toMatchSnapshot();
   })
